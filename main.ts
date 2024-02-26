bcc2d174834d05ea374df859dc6521ac2f289ac7
m4uiiii <156123497+m4uiiii@users.noreply.github.com>
2024-02-26 11:20:24 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+namespace userconfig {
+    export const ARCADE_SCREEN_WIDTH = 240
+    export const ARCADE_SCREEN_HEIGHT = 200
+}
+tiles.setCurrentTilemap(tilemap`level8`)
+scene.setBackgroundColor(9)
+scene.centerCameraAt(18 * 18 / 2, 17 * 17 / 2)
