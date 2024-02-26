5b27200d74971cc1d540be0d54bfedb96ea7c091
m4uiiii <156123497+m4uiiii@users.noreply.github.com>
2024-02-26 12:46:56 -0600

    Incomplete of function of whose turn is it

--- main.ts
+++ main.ts
@@ -1,2 +1,51 @@
-tiles.setCurrentTilemap(tilemap`level8`)
+function Whose_turn_is_it (reds_turn: boolean) {
+    chip_being_placed = true
+    if (reds_turn) {
+        generated_chip = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . 2 2 2 2 2 2 2 2 . . . . 
+            . . . 2 2 2 b b 2 2 2 2 2 . . . 
+            . . . 2 2 b 2 2 2 2 2 2 2 . . . 
+            . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+            . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+            . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+            . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+            . . . . 2 2 2 2 2 2 2 2 . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Projectile)
+    } else {
+        generated_chip = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . 5 5 5 5 5 5 5 5 . . . . 
+            . . . 5 5 5 1 1 5 5 5 5 5 . . . 
+            . . . 5 5 1 5 5 5 5 5 5 5 . . . 
+            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+            . . . . 5 5 5 5 5 5 5 5 . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Player)
+    }
+}
+let generated_chip: Sprite = null
+let chip_being_placed = false
+namespace userconfig{
+    export const ARCADE_SCREEN_WIDTH = 240
+    export const ARCADE_SCREEN_HEIGHT = 200
+}
+tiles.setCurrentTilemap(tilemap`Connect 4 Map`)
+scene.setBackgroundColor(4)
 scene.centerCameraAt(16 * 16 / 2, 16 * 16 / 2)
