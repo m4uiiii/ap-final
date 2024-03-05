40342fef13ce587b2ad2d65fca68c6db56ba9495
m4uiiii <156123497+m4uiiii@users.noreply.github.com>
2024-03-05 13:17:16 -0600

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,17 @@
 namespace SpriteKind {
     export const chip = SpriteKind.create()
 }
+scene.onHitWall(SpriteKind.chip, function (sprite, location) {
+    if (red_turn) {
+
+    } else {
+
+    }
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     generated_chip.vy = 150
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (most_left_column < tiles.locationXY(tiles.locationOfSprite(generated_chip), tiles.XY.column)) {
-        tiles.placeOnTile(generated_chip, tiles.getTileLocation(tiles.locationXY(tiles.locationOfSprite(generated_chip), tiles.XY.column) - 1, 3))
-    }
-})
-function Turn (reds_turn: boolean) {
+function Turn(reds_turn: boolean) {
     chip_being_placed = true
     if (reds_turn) {
         generated_chip = sprites.create(img`
@@ -50,25 +52,36 @@ function Turn (reds_turn: boolean) {
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.chip)
     }
+    for (let value of sprites.allOfKind(SpriteKind.chip)) {
+        if (value.tilemapLocation() == generated_chip.tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom)) {
+            break;
+        }
+    }
     tiles.placeOnTile(generated_chip, tiles.getTileLocation(7, 3))
 }
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (most_left_column < tiles.locationXY(tiles.locationOfSprite(generated_chip), tiles.XY.column)) {
-        tiles.placeOnTile(generated_chip, tiles.getTileLocation(tiles.locationXY(tiles.locationOfSprite(generated_chip), tiles.XY.column) + 1, 3))
+function askQuestion() {
+    question = game.askForNumber("Which connect 4 map? Type1:6x6map Type2:7x7map Type3:8x8map", 1)
+    if (true) {
+        tiles.setCurrentTilemap(tilemap`level10`)
+    } else if (0 == 0) {
+        tiles.setCurrentTilemap(tilemap`level11`)
+    } else {
+        tiles.setCurrentTilemap(tilemap`level12`)
     }
-})
+}
+let question = 0
 let chip_being_placed = false
-let most_left_column = 0
 let generated_chip: Sprite = null
-namespace userconfig{
+let red_turn = false
+let most_left_column = 0
+namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 240
     export const ARCADE_SCREEN_HEIGHT = 200
 }
-let maps = [tilemap`6x6 connect 4`, tilemap`7x7 connect 4`, tilemap`7x7 connect 4`]
+let maps = [tilemap`level13`, tilemap`level14`, tilemap`level15`]
 tiles.setCurrentTilemap(maps._pickRandom())
 scene.setBackgroundColor(4)
 scene.centerCameraAt(15 * 15 / 2, 16 * 16 / 2)
+red_turn = true
+Turn(red_turn)
 Turn(true)
-forever(function () {
-	
-})
