9d5558aa8692dce379c5131c5baaa79a4d7f4075
m4uiiii <156123497+m4uiiii@users.noreply.github.com>
2024-03-18 13:04:21 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,17 @@
 namespace SpriteKind {
     export const chip = SpriteKind.create()
 }
+// Makes is so that the chips don't pass through each other and can stack.
 scene.onHitWall(SpriteKind.chip, function (sprite, location) {
-    if (red_turn) {
-    	
-    } else {
-    	
-    }
+    tiles.setWallAt(sprite.tilemapLocation(), true)
 })
 // drops the chip. Used this from youtube video https://www.youtube.com/live/aum5VUMfeeg?si=Sjh828dOPgUOb2zT
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    generated_chip.vy = 150
-    chip_being_placed = false
+    red_turn = !(red_turn)
+    Turn(red_turn)
 })
+// Allows the chip to move left.
+//  Used this from youtube video https://www.youtube.com/live/aum5VUMfeeg?si=Sjh828dOPgUOb2zT
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     if (chip_being_placed) {
         if (most_left_column < tiles.locationXY(tiles.locationOfSprite(generated_chip), tiles.XY.column)) {
@@ -20,8 +19,17 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         }
     }
 })
+// Calls the generated chip so it can be placed in the game. It also decides whose turn it is or whose turn is next.
 function Turn (reds_turn: boolean) {
     chip_being_placed = true
+    for (let value of sprites.allOfKind(SpriteKind.chip)) {
+        a = value.tilemapLocation().column == generated_chip.tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom).column
+        b = value.tilemapLocation().row == generated_chip.tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom).row
+        if (a && b) {
+            break;
+        }
+    }
+    generated_chip.vy = 150
     if (reds_turn) {
         generated_chip = sprites.create(img`
             . . . . . . . . . . . . . . . . 
@@ -61,13 +69,10 @@ function Turn (reds_turn: boolean) {
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.chip)
     }
-    for (let value of sprites.allOfKind(SpriteKind.chip)) {
-        if (value.tilemapLocation() == generated_chip.tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom)) {
-            break;
-        }
-    }
     tiles.placeOnTile(generated_chip, tiles.getTileLocation(7, 3))
 }
+// Allows the chip to move right.
+//  Used this from youtube video https://www.youtube.com/live/aum5VUMfeeg?si=Sjh828dOPgUOb2zT
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     if (chip_being_placed) {
         if (most_right_column > tiles.locationXY(tiles.locationOfSprite(generated_chip), tiles.XY.column)) {
@@ -87,6 +92,8 @@ function askQuestion () {
     }
 }
 let question = 0
+let b = false
+let a = false
 let chip_being_placed = false
 let generated_chip: Sprite = null
 let red_turn = false
@@ -94,6 +101,7 @@ let most_right_column = 0
 let most_left_column = 0
 most_left_column = 4
 most_right_column = 11
+askQuestion()
 namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 240
     export const ARCADE_SCREEN_HEIGHT = 200
@@ -103,4 +111,22 @@ tiles.setCurrentTilemap(maps._pickRandom())
 scene.setBackgroundColor(4)
 scene.centerCameraAt(15 * 15 / 2, 17 * 17 / 2)
 red_turn = true
-Turn(red_turn)
+generated_chip = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 2 2 2 . . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . 2 2 b b b 2 2 2 2 2 2 2 . . 
+    . 2 2 b b b 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 b 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . . 2 2 2 2 2 2 2 2 . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.chip)
+tiles.placeOnTile(generated_chip, tiles.getTileLocation(7, 3))
